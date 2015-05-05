f1be112df280c6ae8ab47df6b9b10f276df32dd2
mirage335 <spamfreemirage335@gmail.com>
2015-05-05 18:12:54 +0000

    Electronics case fan enabled.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -61,16 +61,17 @@
 //This is for controlling a fan to cool down the stepper drivers
 //it will turn on when any driver is enabled
 //and turn off after the set amount of seconds from last driver being disabled again
-#define CONTROLLERFAN_PIN -1 //Pin used for the fan to cool controller (-1 to disable)
-#define CONTROLLERFAN_SECS 60 //How many seconds, after all motors were disabled, the fan should run
-#define CONTROLLERFAN_SPEED 255  // == full speed
+//#define CONTROLLERFAN_PIN -1 //Pin used for the fan to cool controller (-1 to disable)
+//#define CONTROLLERFAN_SECS 60 //How many seconds, after all motors were disabled, the fan should run
+//#define CONTROLLERFAN_SPEED 255  // == full speed
 
-#define CASEFAN_PIN 2 //Pin used for the fan to cool controller (-1 to disable)
-#define CASEFAN_SECS 15 //How many seconds, after all motors were disabled, the fan should run before going back to idle speed.
-#define CASEFAN_SPEED_FULL 100  // Full speed for when motor are active
-#define CASEFAN_SPEED_IDLE 70          // Idle speed for when the motor have been inactive     
-#define CASEFAN_SPEED_MAX 128          // Maximum limit for the fan speed so it does not burn out. Use 128 for 12v fans with 24V Power Supplies
-#define CASEFAN_SPEED_MIN 70   // Minimum limit for the fan speed where it will start to spin from a stop without a push.
+#define CONTROLLERFAN_PIN 2 //Pin used for the fan to cool controller (-1 to disable)
+#define CONTROLLERFAN_SECS 15 //How many seconds, after all motors were disabled, the fan should run before going back to idle speed.
+#define CONTROLLERFAN_SPEED 128  // == full speed
+#define CONTROLLERFAN_SPEED_FULL 100  // Full speed for when motor are active
+#define CONTROLLERFAN_SPEED_IDLE 70          // Idle speed for when the motor have been inactive     
+#define CONTROLLERFAN_SPEED_MAX 128          // Maximum limit for the fan speed so it does not burn out. Use 128 for 12v fans with 24V Power Supplies
+#define CONTROLLERFAN_SPEED_MIN 70   // Minimum limit for the fan speed where it will start to spin from a stop without a push.
 
 // When first starting the main fan, run it at full speed for the
 // given number of milliseconds.  This gets the fan spinning reliably
